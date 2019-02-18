0961cf534b97ff48658d7f0674bdc84f959cc05c
ashu8912 <ashughildiyal5@gmail.com>
2019-02-18 21:32:31 +0530

    created pre mongoose middleware and hashed passwords in it

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-
+const bcrypt=require("bcryptjs");
 const Schema=mongoose.Schema;
 
 const userSchema=new Schema({
@@ -17,3 +17,25 @@ const userSchema=new Schema({
        required:"password is required"
     },
 })
+userSchema.pre("save",function(next){
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });  
+})
+
+const User=mongoose.model("users",userSchema);
