9196df83d19958699989b1cfaa5a23d92cf961b1
ashu8912 <ashughildiyal5@gmail.com>
2019-02-18 21:30:06 +0530

    created session middleware

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const cors=require("cors");
 const app=express();
+const passport=require("passport");
 
-const routes=require("./routes/route");
-app.use(cors());
+const session=require("express-session");
+const indexRoutes=require("./routes");
+const authRoutes=require("./routes/auth");
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.use("/",routes);
-
+app.use(session({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{expires:24*60*60*1000}
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/api",indexRoutes);
+app.use("/api",authRoutes);
 const errorHandlers=require('./handlers/errorHandler');
 app.get("*",errorHandlers.notFound);
 app.use(errorHandlers.productionErrors);
