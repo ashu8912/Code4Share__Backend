1159f05ab9bc237063f52142f0ca492286b97da3
ashu8912 <ashughildiyal5@gmail.com>
2019-02-21 16:53:49 +0530

    added mongoStore for session storage

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser=require("body-parser");
 const cors=require("cors");
 const app=express();
 const passport=require("passport");
-
+const mongoose=require("mongoose");
 const session=require("express-session");
+const MongoStore = require('connect-mongo')(session);
+ 
 const indexRoutes=require("./routes");
 const authRoutes=require("./routes/auth");
 const taskRoutes=require("./routes/task");
@@ -19,6 +21,7 @@ app.use(session({
     secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
+    store:new MongoStore({mongooseConnection:mongoose.connection}),
     cookie:{expires:24*60*60*1000}
 }))
 app.use(passport.initialize());
@@ -29,13 +32,13 @@ app.use("/api",taskRoutes);
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static('frontend/build'));
+    app.use(express.static('frontend'));
   
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
     });
   }
 const errorHandlers=require('./handlers/errorHandler');
