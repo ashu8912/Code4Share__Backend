4819f53e66890e8d6ec714a599b797f6092f27e6
ashu8912 <ashughildiyal5@gmail.com>
2019-02-21 16:58:58 +0530

    changed to async await

--- services/passport.js
+++ services/passport.js
@@ -2,14 +2,13 @@ const passport=require("passport");
 const mongoose=require("mongoose");
 const User=mongoose.model("users");
 const LocalStrategy=require("passport-local").Strategy;
+const FacebookStrategy=require("passport-facebook").Strategy;
 const bcrypt=require("bcryptjs");
 passport.serializeUser(function(user,done){
 done(null,user._id);
 })
 passport.deserializeUser(function(id,done){
-console.log("id",id)    
 User.findById(id,(err,user)=>{
-    console.log("user",user);
     done(err,user);
 })    
 })
@@ -19,18 +18,48 @@ passport.use(new LocalStrategy({
         passwordField: 'password'
       
 },
-    function(email, password, done) {
-      User.findOne({ email }, function (err, user) {
-        if (err) { return done(err); }
+    async function(email, password, done) {
+       try{      
+        let user=await User.findOne({ email })
         if (!user) { return done(null, false,"no user with this email exists"); }
-        bcrypt.compare(password,user.password).then((value)=>{
+        let value=await bcrypt.compare(password,user.password);
             if(value)
             {
                 return done(null, user);
             }
             return done(null, false,"wrong email or password provided");
-        })
-        
-      });
+}
+    catch(err){
+        return done(err);
     }
-  ));
+}))
+
+  passport.use(new FacebookStrategy({
+  clientID:process.env.FACEBOOK_CLIENT_ID,
+  clientSecret:process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL:process.env.FACEBOOK_CALLBACK_URL,
+  profileFields:["id","displayName","email"]},async function(
+  token,refreshToken,profile,done    
+  ){
+try{      
+let user=await User.findOne({facebookId:profile.id});
+if(user)
+{
+    return done(null,user);
+}
+var newUser=new User();
+    if(profile.emails)
+    {
+        newUser.email=profile.emails[0].value;
+    }
+    newUser.name=profile.displayName; 
+    newUser.facebookId=profile.id;
+    try
+    {let savedUser=await newUser.save()
+        return done(null,savedUser);
+    }
+    catch(err){
+        return done(null,false,"cant save user info")
+    }
+}catch(err){return done(err)}
+  }))
