394baff27ec6dbcb9f90219c02523fe965bdc245
ashu8912 <ashughildiyal5@gmail.com>
2019-02-21 16:59:38 +0530

    final commit

---
+++ frontend/precache-manifest.b3db5064fe6d207e2a866124a171b414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d504dd4ca303bf5b7684",
+    "url": "/static/js/main.d504dd4c.chunk.js"
+  },
+  {
+    "revision": "5a0457d8c1d2c2053d61",
+    "url": "/static/js/2.5a0457d8.chunk.js"
+  },
+  {
+    "revision": "d504dd4ca303bf5b7684",
+    "url": "/static/css/main.cc2ad58a.chunk.css"
+  },
+  {
+    "revision": "1e6a217e4db21e14ca629f883d7700c1",
+    "url": "/index.html"
+  }
+];
